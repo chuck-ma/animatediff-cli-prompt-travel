15e503b7055abf99fbe2e579436f750984c60f6a
chuckma <chuck_ma@qq.com>
2023-10-30 18:25:14 +0800

    add change code

--- src/animatediff/utils/util.py
+++ src/animatediff/utils/util.py
@@ -12,7 +12,9 @@ from tqdm.rich import tqdm
 
 logger = logging.getLogger(__name__)
 
-def save_frames(video: Tensor, frames_dir: PathLike, show_progress:bool=True):
+
+def save_frames(video: Tensor, frames_dir: PathLike, show_progress: bool = True):
+    print("starting save frames...")
     frames_dir = Path(frames_dir)
     frames_dir.mkdir(parents=True, exist_ok=True)
     frames = rearrange(video, "b c t h w -> t b c h w")
@@ -24,11 +26,12 @@ def save_frames(video: Tensor, frames_dir: PathLike, show_progress:bool=True):
             save_image(frame, frames_dir.joinpath(f"{idx:08d}.png"))
 
 
-def save_imgs(imgs:List[Image.Image], frames_dir: PathLike):
+def save_imgs(imgs: List[Image.Image], frames_dir: PathLike):
     frames_dir = Path(frames_dir)
     frames_dir.mkdir(parents=True, exist_ok=True)
     for idx, img in enumerate(tqdm(imgs, desc=f"Saving frames to {frames_dir.stem}")):
-        img.save( frames_dir.joinpath(f"{idx:08d}.png") )
+        img.save(frames_dir.joinpath(f"{idx:08d}.png"))
+
 
 def save_video(video: Tensor, save_path: PathLike, fps: int = 8):
     save_path = Path(save_path)
@@ -64,35 +67,35 @@ def resize_for_condition_image(input_image: Image, us_width: int, us_height: int
     img = input_image.resize((W, H), resample=Image.LANCZOS)
     return img
 
-def get_resized_images(org_images_path: List[str], us_width: int, us_height: int):
 
-    images = [Image.open( p ) for p in org_images_path]
+def get_resized_images(org_images_path: List[str], us_width: int, us_height: int):
+    images = [Image.open(p) for p in org_images_path]
 
     W, H = images[0].size
 
     if us_width == -1:
-        us_width = W/H * us_height
+        us_width = W / H * us_height
     elif us_height == -1:
-        us_height = H/W * us_width
+        us_height = H / W * us_width
 
     return [resize_for_condition_image(img, us_width, us_height) for img in images]
 
-def get_resized_image(org_image_path: str, us_width: int, us_height: int):
 
-    image = Image.open( org_image_path )
+def get_resized_image(org_image_path: str, us_width: int, us_height: int):
+    image = Image.open(org_image_path)
 
     W, H = image.size
 
     if us_width == -1:
-        us_width = W/H * us_height
+        us_width = W / H * us_height
     elif us_height == -1:
-        us_height = H/W * us_width
+        us_height = H / W * us_width
 
     return resize_for_condition_image(image, us_width, us_height)
 
-def get_resized_image2(org_image_path: str, size: int):
 
-    image = Image.open( org_image_path )
+def get_resized_image2(org_image_path: str, size: int):
+    image = Image.open(org_image_path)
 
     W, H = image.size
 
@@ -101,9 +104,9 @@ def get_resized_image2(org_image_path: str, size: int):
 
     if W < H:
         us_width = size
-        us_height = int(size * H/W)
+        us_height = int(size * H / W)
     else:
-        us_width = int(size * W/H)
+        us_width = int(size * W / H)
         us_height = size
 
     return resize_for_condition_image(image, us_width, us_height)
@@ -111,6 +114,8 @@ def get_resized_image2(org_image_path: str, size: int):
 
 def show_gpu(comment):
     pass
+
+
 #    import GPUtil
 #    torch.cuda.synchronize()
 #    logger.info(comment)
@@ -118,6 +123,7 @@ def show_gpu(comment):
 
 
 PROFILE_ON = False
+
 
 def start_profile():
     if PROFILE_ON:
@@ -129,6 +135,7 @@ def start_profile():
     else:
         return None
 
+
 def end_profile(pr, file_name):
     if PROFILE_ON:
         import io
@@ -136,19 +143,21 @@ def end_profile(pr, file_name):
 
         pr.disable()
         s = io.StringIO()
-        ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
+        ps = pstats.Stats(pr, stream=s).sort_stats("cumtime")
         ps.print_stats()
 
-        with open(file_name, 'w+') as f:
+        with open(file_name, "w+") as f:
             f.write(s.getvalue())
+
 
 STOPWATCH_ON = False
 
 time_record = []
 start_time = 0
 
+
 def stopwatch_start():
-    global start_time,time_record
+    global start_time, time_record
     import time
 
     if STOPWATCH_ON:
@@ -156,15 +165,16 @@ def stopwatch_start():
         torch.cuda.synchronize()
         start_time = time.time()
 
+
 def stopwatch_record(comment):
     import time
 
     if STOPWATCH_ON:
         torch.cuda.synchronize()
-        time_record.append(((time.time() - start_time) , comment))
+        time_record.append(((time.time() - start_time), comment))
+
 
 def stopwatch_stop(comment):
-
     if STOPWATCH_ON:
         stopwatch_record(comment)
 
@@ -185,7 +195,7 @@ def prepare_ip_adapter():
         "models/ip-adapter-plus_sd15.bin",
         "models/ip-adapter_sd15.bin",
         "models/ip-adapter_sd15_light.bin",
-        "models/ip-adapter-plus-face_sd15.bin"
+        "models/ip-adapter-plus-face_sd15.bin",
     ]:
         path = Path(hub_file)
 
@@ -195,8 +205,12 @@ def prepare_ip_adapter():
             continue
 
         hf_hub_download(
-            repo_id="h94/IP-Adapter", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/ip_adapter"
+            repo_id="h94/IP-Adapter",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/ip_adapter",
         )
+
 
 def prepare_motion_module():
     import os
@@ -216,8 +230,12 @@ def prepare_motion_module():
             continue
 
         hf_hub_download(
-            repo_id="guoyww/animatediff", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/motion-module"
+            repo_id="guoyww/animatediff",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/motion-module",
         )
+
 
 def prepare_wd14tagger():
     import os
@@ -238,8 +256,12 @@ def prepare_wd14tagger():
             continue
 
         hf_hub_download(
-            repo_id="SmilingWolf/wd-v1-4-moat-tagger-v2", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/WD14tagger"
+            repo_id="SmilingWolf/wd-v1-4-moat-tagger-v2",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/WD14tagger",
         )
+
 
 def prepare_dwpose():
     import os
@@ -260,9 +282,11 @@ def prepare_dwpose():
             continue
 
         hf_hub_download(
-            repo_id="yzd-v/DWPose", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/DWPose"
+            repo_id="yzd-v/DWPose",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/DWPose",
         )
-
 
 
 def prepare_softsplat():
@@ -283,7 +307,10 @@ def prepare_softsplat():
             continue
 
         hf_hub_download(
-            repo_id="s9roll74/softsplat_mirror", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/softsplat"
+            repo_id="s9roll74/softsplat_mirror",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/softsplat",
         )
 
 
@@ -291,44 +318,43 @@ def extract_frames(movie_file_path, fps, out_dir, aspect_ratio, duration, offset
     import ffmpeg
 
     probe = ffmpeg.probe(movie_file_path)
-    video = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-    width = int(video['width'])
-    height = int(video['height'])
+    video = next((stream for stream in probe["streams"] if stream["codec_type"] == "video"), None)
+    width = int(video["width"])
+    height = int(video["height"])
 
-    node = ffmpeg.input( str(movie_file_path.resolve()) )
+    node = ffmpeg.input(str(movie_file_path.resolve()))
 
-    node = node.filter( "fps", fps=fps )
-
+    node = node.filter("fps", fps=fps)
 
     if duration > 0:
-        node = node.trim(start=offset,end=offset+duration).setpts('PTS-STARTPTS')
+        node = node.trim(start=offset, end=offset + duration).setpts("PTS-STARTPTS")
     elif offset > 0:
-        node = node.trim(start=offset).setpts('PTS-STARTPTS')
+        node = node.trim(start=offset).setpts("PTS-STARTPTS")
 
     if size_of_short_edge != -1:
         if width < height:
             r = height / width
             width = size_of_short_edge
-            height = int( (size_of_short_edge * r)//8 * 8)
-            node = node.filter('scale', size_of_short_edge, height)
+            height = int((size_of_short_edge * r) // 8 * 8)
+            node = node.filter("scale", size_of_short_edge, height)
         else:
             r = width / height
             height = size_of_short_edge
-            width = int( (size_of_short_edge * r)//8 * 8)
-            node = node.filter('scale', width, size_of_short_edge)
+            width = int((size_of_short_edge * r) // 8 * 8)
+            node = node.filter("scale", width, size_of_short_edge)
 
     if aspect_ratio > 0:
         # aspect ratio (width / height)
         ww = round(height * aspect_ratio)
         if ww < width:
-            x= (width - ww)//2
-            y= 0
+            x = (width - ww) // 2
+            y = 0
             w = ww
             h = height
         else:
-            hh = round(width/aspect_ratio)
+            hh = round(width / aspect_ratio)
             x = 0
-            y = (height - hh)//2
+            y = (height - hh) // 2
             w = width
             h = hh
         w = int(w // 8 * 8)
@@ -336,21 +362,19 @@ def extract_frames(movie_file_path, fps, out_dir, aspect_ratio, duration, offset
         logger.info(f"crop to {w=},{h=}")
         node = node.crop(x, y, w, h)
 
-    node = node.output( str(out_dir.resolve().joinpath("%08d.png")), start_number=0 )
+    node = node.output(str(out_dir.resolve().joinpath("%08d.png")), start_number=0)
 
     node.run(quiet=True, overwrite_output=True)
 
 
-
-
-
-
-def is_v2_motion_module(motion_module_path:Path):
+def is_v2_motion_module(motion_module_path: Path):
     if motion_module_path.suffix == ".safetensors":
         from safetensors.torch import load_file
+
         loaded = load_file(motion_module_path, "cpu")
     else:
         from torch import load
+
         loaded = load(motion_module_path, "cpu")
 
     is_v2 = "mid_block.motion_modules.0.temporal_transformer.norm.bias" in loaded
@@ -363,32 +387,31 @@ def is_v2_motion_module(motion_module_path:Path):
     return is_v2
 
 
-
-
 tensor_interpolation = None
+
 
 def get_tensor_interpolation_method():
     return tensor_interpolation
+
 
 def set_tensor_interpolation_method(is_slerp):
     global tensor_interpolation
     tensor_interpolation = slerp if is_slerp else linear
 
+
 def linear(v1, v2, t):
     return (1.0 - t) * v1 + t * v2
 
-def slerp(
-    v0: torch.Tensor, v1: torch.Tensor, t: float, DOT_THRESHOLD: float = 0.9995
-) -> torch.Tensor:
+
+def slerp(v0: torch.Tensor, v1: torch.Tensor, t: float, DOT_THRESHOLD: float = 0.9995) -> torch.Tensor:
     u0 = v0 / v0.norm()
     u1 = v1 / v1.norm()
     dot = (u0 * u1).sum()
     if dot.abs() > DOT_THRESHOLD:
-        #logger.info(f'warning: v0 and v1 close to parallel, using linear interpolation instead.')
+        # logger.info(f'warning: v0 and v1 close to parallel, using linear interpolation instead.')
         return (1.0 - t) * v0 + t * v1
     omega = dot.acos()
     return (((1.0 - t) * omega).sin() * v0 + (t * omega).sin() * v1) / omega.sin()
-
 
 
 def prepare_sam_hq(low_vram):
@@ -398,9 +421,7 @@ def prepare_sam_hq(low_vram):
     from huggingface_hub import hf_hub_download
 
     os.makedirs("data/models/SAM", exist_ok=True)
-    for hub_file in [
-        "sam_hq_vit_h.pth" if not low_vram else "sam_hq_vit_b.pth"
-    ]:
+    for hub_file in ["sam_hq_vit_h.pth" if not low_vram else "sam_hq_vit_b.pth"]:
         path = Path(hub_file)
 
         saved_path = "data/models/SAM" / path
@@ -409,8 +430,12 @@ def prepare_sam_hq(low_vram):
             continue
 
         hf_hub_download(
-            repo_id="lkeab/hq-sam", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/SAM"
+            repo_id="lkeab/hq-sam",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/SAM",
         )
+
 
 def prepare_groundingDINO():
     import os
@@ -430,7 +455,10 @@ def prepare_groundingDINO():
             continue
 
         hf_hub_download(
-            repo_id="ShilongLiu/GroundingDINO", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/GroundingDINO"
+            repo_id="ShilongLiu/GroundingDINO",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/GroundingDINO",
         )
 
 
@@ -443,7 +471,11 @@ def prepare_propainter():
         if os.listdir("src/animatediff/repo/ProPainter"):
             return
 
-    repo = git.Repo.clone_from(url="https://github.com/sczhou/ProPainter", to_path="src/animatediff/repo/ProPainter", no_checkout=True )
+    repo = git.Repo.clone_from(
+        url="https://github.com/sczhou/ProPainter",
+        to_path="src/animatediff/repo/ProPainter",
+        no_checkout=True,
+    )
     repo.git.checkout("a8a5827ca5e7e8c1b4c360ea77cbb2adb3c18370")
 
 
@@ -465,5 +497,8 @@ def prepare_anime_seg():
             continue
 
         hf_hub_download(
-            repo_id="skytnt/anime-seg", subfolder=PurePosixPath(path.parent), filename=PurePosixPath(path.name), local_dir="data/models/anime_seg"
+            repo_id="skytnt/anime-seg",
+            subfolder=PurePosixPath(path.parent),
+            filename=PurePosixPath(path.name),
+            local_dir="data/models/anime_seg",
         )
